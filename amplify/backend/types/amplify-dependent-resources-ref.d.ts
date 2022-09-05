0ad7fe2c20a4ee641eab66d0e659b9964406ca38
yanasekenta <ynsknt0505@gmail.com>
2022-09-05 18:38:10 +0900

    apiを編集してpush

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -12,6 +12,7 @@ export type AmplifyDependentResourcesAttributes = {
     },
     "api": {
         "reactionic": {
+            "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
